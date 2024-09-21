c3c87fc00f26d0435022c49cc13d241326e461b4
sey323 <sey323_nm@yahoo.co.jp>
2024-09-21 17:15:47 +0900

    :up: tailwindcssのインストール

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,16 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxtjs/supabase"],
+  css: ["~/assets/css/main.css"],
   runtimeConfig: {
     public: {
       redirectUrl: process.env.NEXT_PUBLIC_REDIRECT_URL,
+    },
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
     },
   },
 });
