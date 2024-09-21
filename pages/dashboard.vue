39407dd30159ed7c5ed47d649e5298dcfe612e2b
sayako21 <>
2024-09-21 21:22:22 +0900

    [追加]ダッシュボード追加

---
+++ pages/dashboard.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+const houseWorkList = [
+  { label: "今日やった洗濯", imageSrc: "/img/souhuki.png" },
+  { label: "今日キレイになった風呂", imageSrc: "/img/hurosouji.webp" },
+  { label: "今日キレイになった部屋", imageSrc: "/img/heyasouji.webp" },
+  { label: "今日キレイになった食器", imageSrc: "/img/saraarai.webp" },
+  { label: "今日キレイになったトイレ", imageSrc: "/img/toire.webp" },
+  { label: "今日つくった料理", imageSrc: "/img/ryouri.webp" },
+];
+</script>
+
+<template>
+  <div
+    v-for="work in houseWorkList"
+    :key="work.label"
+    class="relative mx-auto my-5 max-w-xs p-4 overflow-hidden bg-white shadow-lg"
+  >
+    <img
+      :src="work.imageSrc"
+      alt="btc logo"
+      class="absolute w-24 h-24 opacity-50 -top-6 -right-6 md:-right-4"
+    />
+    <div class="w-full">
+      <p class="mb-1 text-xl font-medium text-gray-800">
+        {{ work.label }}
+      </p>
+      <p class="text-5xl font-extrabold mb-2">12345</p>
+      <div class="flex items-center justify-between">
+        <p>Task progress</p>
+        <p>33%</p>
+      </div>
+      <div class="w-full h-2 mt-3 mb-6 bg-gray-400 rounded-full">
+        <div
+          class="w-1/3 h-full text-center text-xs text-white bg-green-400 rounded-full"
+        ></div>
+      </div>
+      <div class="flex items-center">
+        <div class="flex -space-x-2">
+          <a href="#" class="">
+            <img
+              class="inline-block h-10 w-10 rounded-full object-cover ring-2 ring-white"
+              src="/img/ryouri.webp"
+              alt="Guy"
+            />
+          </a>
+          <a href="#" class="">
+            <img
+              class="inline-block h-10 w-10 rounded-full object-cover ring-2 ring-white"
+              src="/img/ryouri.webp"
+              alt="Max"
+            />
+          </a>
+          <a href="#" class="">
+            <img
+              src="/img/ryouri.webp"
+              class="inline-block h-10 w-10 rounded-full object-cover ring-2 ring-white"
+              alt="Charles"
+            />
+          </a>
+          <a href="#" class="">
+            <img
+              class="inline-block h-10 w-10 rounded-full object-cover ring-2 ring-white"
+              src="/img/ryouri.webp"
+              alt="Jade"
+            />
+          </a>
+        </div>
+        <span class="ml-2 text-gray-500"> + 4 more </span>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -39,68 +39,67 @@
     label="＋"
     @click="navigateToAddWork"
   />
-  <PrimaryButton label="投稿する" @click="navigateToAddWork" />
 </template>
 
 <script setup lang="ts">
-  // supabaseからログアウト
-  const client = useSupabaseClient();
-  const user = useSupabaseUser();
-  import dayjs from "dayjs";
-  import relativeTimePlugin from "dayjs/plugin/relativeTime";
+// supabaseからログアウト
+const client = useSupabaseClient();
+const user = useSupabaseUser();
+import dayjs from "dayjs";
+import relativeTimePlugin from "dayjs/plugin/relativeTime";
 
-  var kajis = ref([]) as any;
+var kajis = ref([]) as any;
 
-  // 初期表示時に家事一覧を取得
-  onMounted(() => {
-    fetchKajis();
-  });
+// 初期表示時に家事一覧を取得
+onMounted(() => {
+  fetchKajis();
+});
 
-  const fetchKajis = async () => {
-    const { data } = await client.from("done_house_work").select("*");
-    kajis.value = data;
-  };
+const fetchKajis = async () => {
+  const { data } = await client.from("done_house_work").select("*");
+  kajis.value = data;
+};
 
-  // relativeTimeプラグインを有効化
-  dayjs.extend(relativeTimePlugin);
+// relativeTimeプラグインを有効化
+dayjs.extend(relativeTimePlugin);
 
-  // 現在の時刻との差を計算して相対時間を返す関数
-  const relativeTimeInMinutes = (time: string): string => {
-    const now = dayjs();
-    const diffMinutes = now.diff(dayjs(time), "minute");
-    const diffHours = now.diff(dayjs(time), "hour");
+// 現在の時刻との差を計算して相対時間を返す関数
+const relativeTimeInMinutes = (time: string): string => {
+  const now = dayjs();
+  const diffMinutes = now.diff(dayjs(time), "minute");
+  const diffHours = now.diff(dayjs(time), "hour");
 
-    if (diffMinutes < 1) {
-      return "たった今"; // 1分未満の場合
-    } else if (diffHours < 1) {
-      return `${diffMinutes} 分前`; // 1時間未満の場合
-    } else {
-      return `${diffHours} 時間前`; // 1時間以上の場合
-    }
-  };
+  if (diffMinutes < 1) {
+    return "たった今"; // 1分未満の場合
+  } else if (diffHours < 1) {
+    return `${diffMinutes} 分前`; // 1時間未満の場合
+  } else {
+    return `${diffHours} 時間前`; // 1時間以上の場合
+  }
+};
 
-  const navigateToAddWork = () => {
-    navigateTo("/add-house-work");
-  };
+const navigateToAddWork = () => {
+  navigateTo("/add-house-work");
+};
 </script>
 
 <style scoped>
-  .floating-button {
-    position: fixed; /* 固定 */
-    bottom: 70px; /* 下からの距離 */
-    right: 20px; /* 右からの距離 */
-    color: white; /* ボタンの文字色 */
-    border: none; /* ボーダーをなしに */
-    border-radius: 50%; /* 丸いボタンに */
-    width: 60px; /* ボタンの幅 */
-    height: 60px; /* ボタンの高さ */
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3); /* 影を追加 */
-    cursor: pointer; /* ポインタを変更 */
-    display: flex; /* フレックスボックス */
-    align-items: center; /* 縦中央揃え */
-    justify-content: center; /* 横中央揃え */
-    transition: background-color 0.3s; /* ホバー時のアニメーション */
-    font-size: 2rem;
-    font-weight: bold;
-  }
+.floating-button {
+  position: fixed; /* 固定 */
+  bottom: 70px; /* 下からの距離 */
+  right: 20px; /* 右からの距離 */
+  color: white; /* ボタンの文字色 */
+  border: none; /* ボーダーをなしに */
+  border-radius: 50%; /* 丸いボタンに */
+  width: 60px; /* ボタンの幅 */
+  height: 60px; /* ボタンの高さ */
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3); /* 影を追加 */
+  cursor: pointer; /* ポインタを変更 */
+  display: flex; /* フレックスボックス */
+  align-items: center; /* 縦中央揃え */
+  justify-content: center; /* 横中央揃え */
+  transition: background-color 0.3s; /* ホバー時のアニメーション */
+  font-size: 2rem;
+  font-weight: bold;
+}
 </style>
