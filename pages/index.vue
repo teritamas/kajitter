f39a15fe6c3fd4a18f244de5c94e0f3651a093e8
Ichiro.S <sey323@users.noreply.github.com>
2024-09-21 20:16:52 +0900

    マージの不具合修正 (#10)

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,7 @@
             <a href="#" class="relative block">
               <img
                 alt="profil"
-                src="@/assets/img/toire.webp"
+                src="/img/toire.webp"
                 class="mx-auto object-cover rounded-full h-10 w-10"
               />
             </a>
@@ -34,90 +34,73 @@
     </ul>
   </div>
 
-  <PrimaryButton class="floating-button" label="＋" @click="postKaji" />
+  <PrimaryButton
+    class="floating-button"
+    label="＋"
+    @click="navigateToAddWork"
+  />
   <PrimaryButton label="投稿する" @click="navigateToAddWork" />
-
 </template>
 
 <script setup lang="ts">
-// supabaseからログアウト
-const client = useSupabaseClient();
-const user = useSupabaseUser();
-import dayjs from "dayjs";
-import relativeTimePlugin from "dayjs/plugin/relativeTime";
+  // supabaseからログアウト
+  const client = useSupabaseClient();
+  const user = useSupabaseUser();
+  import dayjs from "dayjs";
+  import relativeTimePlugin from "dayjs/plugin/relativeTime";
 
-var kajis = ref([]) as any;
+  var kajis = ref([]) as any;
 
-// 初期表示時に家事一覧を取得
-onMounted(() => {
-  fetchKajis();
-});
-
-const fetchKajis = async () => {
-  const { data } = await client.from("done_house_work").select("*");
-  kajis.value = data;
-};
-
-const postKaji = async () => {
-  // 投稿処理
-
-  const { data, error } = await client.from("done_house_work").insert([
-    {
-      user_id: user.value?.id,
-      house_work_name: "洗濯",
-    } as never,
-  ]);
-
-  if (error) {
-    console.error(error);
-    return;
-  } else {
-    console.log("登録完了");
+  // 初期表示時に家事一覧を取得
+  onMounted(() => {
     fetchKajis();
-  }
-};
+  });
 
-// relativeTimeプラグインを有効化
-dayjs.extend(relativeTimePlugin);
+  const fetchKajis = async () => {
+    const { data } = await client.from("done_house_work").select("*");
+    kajis.value = data;
+  };
 
-// 現在の時刻との差を計算して相対時間を返す関数
-const relativeTimeInMinutes = (time: string): string => {
-  const now = dayjs();
-  const diffMinutes = now.diff(dayjs(time), "minute");
-  const diffHours = now.diff(dayjs(time), "hour");
+  // relativeTimeプラグインを有効化
+  dayjs.extend(relativeTimePlugin);
 
-  if (diffMinutes < 1) {
-    return "たった今"; // 1分未満の場合
-  } else if (diffHours < 1) {
-    return `${diffMinutes} 分前`; // 1時間未満の場合
-  } else {
-    return `${diffHours} 時間前`; // 1時間以上の場合
-  }
-};
+  // 現在の時刻との差を計算して相対時間を返す関数
+  const relativeTimeInMinutes = (time: string): string => {
+    const now = dayjs();
+    const diffMinutes = now.diff(dayjs(time), "minute");
+    const diffHours = now.diff(dayjs(time), "hour");
+
+    if (diffMinutes < 1) {
+      return "たった今"; // 1分未満の場合
+    } else if (diffHours < 1) {
+      return `${diffMinutes} 分前`; // 1時間未満の場合
+    } else {
+      return `${diffHours} 時間前`; // 1時間以上の場合
+    }
+  };
 
   const navigateToAddWork = () => {
     navigateTo("/add-house-work");
   };
-
 </script>
 
 <style scoped>
-.floating-button {
-  position: fixed; /* 固定 */
-  bottom: 70px; /* 下からの距離 */
-  right: 20px; /* 右からの距離 */
-  color: white; /* ボタンの文字色 */
-  border: none; /* ボーダーをなしに */
-  border-radius: 50%; /* 丸いボタンに */
-  width: 60px; /* ボタンの幅 */
-  height: 60px; /* ボタンの高さ */
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3); /* 影を追加 */
-  cursor: pointer; /* ポインタを変更 */
-  display: flex; /* フレックスボックス */
-  align-items: center; /* 縦中央揃え */
-  justify-content: center; /* 横中央揃え */
-  transition: background-color 0.3s; /* ホバー時のアニメーション */
-  font-size: 2rem;
-  font-weight: bold;
-}
+  .floating-button {
+    position: fixed; /* 固定 */
+    bottom: 70px; /* 下からの距離 */
+    right: 20px; /* 右からの距離 */
+    color: white; /* ボタンの文字色 */
+    border: none; /* ボーダーをなしに */
+    border-radius: 50%; /* 丸いボタンに */
+    width: 60px; /* ボタンの幅 */
+    height: 60px; /* ボタンの高さ */
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3); /* 影を追加 */
+    cursor: pointer; /* ポインタを変更 */
+    display: flex; /* フレックスボックス */
+    align-items: center; /* 縦中央揃え */
+    justify-content: center; /* 横中央揃え */
+    transition: background-color 0.3s; /* ホバー時のアニメーション */
+    font-size: 2rem;
+    font-weight: bold;
+  }
 </style>
