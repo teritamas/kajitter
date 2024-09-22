fbcf51b402d7c92b8797c4d14b0ac65858c7d4ec
sey323 <sey323_nm@yahoo.co.jp>
2024-09-22 12:05:03 +0900

    :+1: aタグをrouterLinkに変更

--- components/app-footer.vue
+++ components/app-footer.vue
@@ -2,9 +2,9 @@
   <footer class="bg-white dark:bg-gray-800 w-screen py-4">
     <div class="max-w-screen-xl px-4 mx-auto">
       <div class="flex max-w-xs mx-auto items-center justify-between">
-        <a
-          href="/"
-          class="text-gray-400 transition-colors duration-200 hover:text-gray-800 dark:hover:text-200"
+        <routerLink
+          to="/"
+          class="flex items-center justify-center text-gray-400 transition-colors duration-200 hover:text-gray-800 dark:hover:text-200"
         >
           <svg
             width="20"
@@ -18,10 +18,11 @@
               d="M40 48C26.7 48 16 58.7 16 72l0 48c0 13.3 10.7 24 24 24l48 0c13.3 0 24-10.7 24-24l0-48c0-13.3-10.7-24-24-24L40 48zM192 64c-17.7 0-32 14.3-32 32s14.3 32 32 32l288 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L192 64zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32l288 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-288 0zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32l288 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-288 0zM16 232l0 48c0 13.3 10.7 24 24 24l48 0c13.3 0 24-10.7 24-24l0-48c0-13.3-10.7-24-24-24l-48 0c-13.3 0-24 10.7-24 24zM40 368c-13.3 0-24 10.7-24 24l0 48c0 13.3 10.7 24 24 24l48 0c13.3 0 24-10.7 24-24l0-48c0-13.3-10.7-24-24-24l-48 0z"
             />
           </svg>
-        </a>
-        <a
-          href="/dashboard"
-          class="text-gray-400 transition-colors duration-200 hover:text-gray-800 dark:hover:text-200"
+          <p class="ml-2 text-sm">みんなの家事</p>
+        </routerLink>
+        <router-link
+          to="/dashboard"
+          class="flex items-center justify-center text-gray-400 transition-colors duration-200 hover:text-gray-800 dark:hover:text-200"
         >
           <svg
             width="20"
@@ -35,7 +36,8 @@
               d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zm-312 8l0 64c0 13.3 10.7 24 24 24s24-10.7 24-24l0-64c0-13.3-10.7-24-24-24s-24 10.7-24 24zm80-96l0 160c0 13.3 10.7 24 24 24s24-10.7 24-24l0-160c0-13.3-10.7-24-24-24s-24 10.7-24 24zm80 64l0 96c0 13.3 10.7 24 24 24s24-10.7 24-24l0-96c0-13.3-10.7-24-24-24s-24 10.7-24 24z"
             />
           </svg>
-        </a>
+          <p class="ml-2 text-sm">みんなのがんばり</p>
+        </router-link>
       </div>
     </div>
   </footer>
