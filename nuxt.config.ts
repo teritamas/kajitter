ffcb9ca117fdff968b1b70a33715be9f1d3126f1
sey323 <sey323_nm@yahoo.co.jp>
2024-09-22 10:24:00 +0900

    :art: faviconとtitleの更新

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Kajitter",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+    },
+  },
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxtjs/supabase"],
